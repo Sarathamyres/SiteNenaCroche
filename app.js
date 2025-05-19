739d250160f9eeb1cf9a1610f4841bae6091c68b
Sarathamyres <thamyressara071@gmail.com>
2025-05-19 20:52:20 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Glide(".carrossel", {
+    type: "carousel",
+    startAt: 0,
+    perView: 4,
+    autoplay: 2000,
+    hoverpause: true,
+    animationDuration: 800,
+    breakpoints: {
+      400: {
+        perView: 1,
+      },
+      768: {
+        perView: 2,
+      },
+    },
+  }).mount();
+});
+
+const hamburguer = document.querySelector(".hamburger");
+const menu_hamburguer = document.querySelector(".menu-hamburguer");
+
+function toggleMenu() {
+  const isActive = hamburguer.classList.contains("is-active");
+  menu_hamburguer.style.display = isActive ? "block" : "none";
+}
+hamburguer.addEventListener("click", () => {
+  hamburguer.classList.toggle("is-active");
+  toggleMenu();
+});
+
+const informacoes_site = document.querySelector(".informacoes-site h4");
+const infors_container = document.querySelector(".infors");
+informacoes_site.addEventListener("click", () => {
+  infors_container.style.display = "block";
+});
+ function institucionalButton() {
+        infors_container.style.display = "none";
+    }
+
+window.addEventListener("scroll", () => {
+  const iconFixed = document.querySelector(".icons-fixed");
+  if (window.scrollY <= 2) {
+    iconFixed.style.display = "none";
+  } else {
+    iconFixed.style.display = "flex";
+  }
+  if (window.scrollY > 0 && hamburguer.classList.contains("is-active")) {
+    hamburguer.classList.remove("is-active");
+    toggleMenu();
+  }
+});
+
+let radioS = document.querySelectorAll(".nav-produtos input");
+let produtos = document.querySelectorAll(".produtos-container .produtos");
+
+radioS.forEach((radio) => {
+  radio.addEventListener("change", function () {
+    const valueCheck = this.value;
+    produtos.forEach((produto) => {
+      const valueProd = produto.dataset.categoria;
+
+      if (valueCheck === valueProd) {
+        produto.style.display = "flex";
+      } else {
+        produto.style.display = "none";
+      }
+    });
+  });
+});
+document
+  .querySelector(".nav-produtos input:checked")
+  ?.dispatchEvent(new Event("change"));
